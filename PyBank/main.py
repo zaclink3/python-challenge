8b588387476ff864b898b8f920b7d149d4b443bd
zaclink3 <118675689+zaclink3@users.noreply.github.com>
2023-01-07 14:48:17 -0500

    third commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,3 +6,25 @@ pybank_csv= os.path.join("..","Resources","budget_data.csv")
 print("Financial Analysis")
 print("__________________________________________")
 
+month = []
+profit_losses = []
+
+with open(pybank_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimeter=",")
+
+    csv_header = next(csvfile)
+    print(f"Header:{csv_header}")
+
+    for row in csvreader:
+        month = (row [0])
+        profit_losses = (row [1])
+
+
+        
+
+
+
+       list.count(month)= count(row[0])
+       print("Total Months" "=" total_months)
+
+
