d798d46d941dc12d7f5c29d1b239e12b290f191b
zaclink3 <118675689+zaclink3@users.noreply.github.com>
2023-01-08 18:30:54 -0500

    First committe

---
+++ PyPoll/main.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+csvpath = os.path.join("Resources","election_data.csv")
+
+with open(csvpath) as csvfile:
+    csv_reader = csv.reader(csvfile, delimeter=",")
+    
+    total_votes_count = -1
+
+    Charles_count = Diana_count = Raymon_count = 0
+
+    Charles_percent = Diana_percent = Raymon_percent = 0
+
+    for row in csv_reader:
+
+        total_votes_count += 1
+
+        if row[2] == "Charles Casper Stockham":
+            Charles_count +=1
+        elif row[2] == "Diana DeGette":
+            Diana_count +=1
+        elif row[2] == "Raymon Anthony Doane":
+            Raymon_count +=1
+
+    Results = {"Charles Casper Stockham":Charles_count, "Diana DeGette":Diana_count, "Raymon Anthony Doane":Raymon_count}
+
+    Charles_percent = round((Charles_count/total_votes_count)*100, 3)
+    Diana_percent = round((Diana_count/total_votes_count)*100, 3)
+    Raymon_percent = round((Raymon_count/total_votes_count)*100, 3)
+
+    Winner = max(Results, key=Results.get)
+
+printresults = f"""Election Results
+___________________________________
+Total Votes: {total_votes_count}
+___________________________________
+Charles Casper Stockham: {Charles_percent}% ({Charles_count})
+Diana Degette: {Diana_percent}% ({Diana_count})
+Raymon Anthony Doane: {Raymon_percent}% ({Raymon_count})
+___________________________________
+Winner: {Winner}
+___________________________________"""
+
+print(printresults)
