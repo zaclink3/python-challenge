41c22a8f0e74c8b80bfe52f07319f9208ecc1c11
zaclink3 <118675689+zaclink3@users.noreply.github.com>
2023-01-08 17:47:17 -0500

    commit 4

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,43 @@
 import os
 import csv
+import math
 
-pybank_csv= os.path.join("..","Resources","budget_data.csv")
+
+pybank_csv= os.path.join("Resources","budget_data.csv")
 
 print("Financial Analysis")
 print("__________________________________________")
 
 month = []
 profit_losses = []
+change_pl = []
 
 with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimeter=",")
+    month_of_increase = 0
+    greatestincrease = 0
+    greatestdecrease = 0 
+    month_of_decrease = 0
 
-    csv_header = next(csvfile)
-    print(f"Header:{csv_header}")
+    change_pl.append(row[2])
+
+    next(csvreader)
+    
 
     for row in csvreader:
-        month = (row [0])
-        profit_losses = (row [1])
+        print("Total Months:", len(month))
+        print("Total:", sum(profit_losses))
+        print("Average Change:", sum(change_pl)/len(change_pl))
 
-
+        if float(row[1])> greatestincrease:
+            month_of_increase, greatestincrease = row[0],float(row[1])
         
+        if float(row[1])< greatestdecrease:
+            month_of_decrease, greatestdecrease = row[0],float(row[1])
 
 
-
-       list.count(month)= count(row[0])
-       print("Total Months" "=" total_months)
+print(f'Greatest increase in profit: {month_of_increase} , ({greatestincrease})')
+print(f'Greatest decrease in profit: {month_of_decrease} , ({greatestdecrease})')
+     
 
 
